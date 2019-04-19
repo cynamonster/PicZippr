7b4d5a786343199dc0e1d21b9658c8e5f62e7a62
Ben Cynamon <ben.cynamon@gmail.com>
2019-04-19 13:12:50 -0400

    Updates

---
+++ precache-manifest.cba824328ace01fc82ac1a97bca815ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da01364e75b7a884aa5b",
+    "url": "/pics/static/css/main.48afcfdb.chunk.css"
+  },
+  {
+    "revision": "da01364e75b7a884aa5b",
+    "url": "/pics/static/js/main.b58970e0.chunk.js"
+  },
+  {
+    "revision": "e4b61d2902f13ed86345",
+    "url": "/pics/static/js/runtime~main.f694050d.js"
+  },
+  {
+    "revision": "5492c98a24ed432cbe2a",
+    "url": "/pics/static/js/2.b4379b99.chunk.js"
+  },
+  {
+    "revision": "292dbd62b65a738c7a32f8ab3551da3f",
+    "url": "/pics/index.html"
+  }
+];
