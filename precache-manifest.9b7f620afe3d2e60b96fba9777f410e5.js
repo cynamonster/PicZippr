5d753303b003425acf23ba3e5366376c74a248fd
cynamonster <ben.cynamon@gmail.com>
2020-02-21 22:54:17 -0500

    Updates

---
+++ precache-manifest.9b7f620afe3d2e60b96fba9777f410e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f746cbb9274dbb196d1c",
+    "url": "/PicZippr/static/js/runtime~main.ad44effa.js"
+  },
+  {
+    "revision": "626ffabd50c4d511ccc8",
+    "url": "/PicZippr/static/js/main.d999c7ec.chunk.js"
+  },
+  {
+    "revision": "0577d6971cbb034c6130",
+    "url": "/PicZippr/static/js/2.6fdcfe8b.chunk.js"
+  },
+  {
+    "revision": "626ffabd50c4d511ccc8",
+    "url": "/PicZippr/static/css/main.aa976d64.chunk.css"
+  },
+  {
+    "revision": "724a56e75b2f29933a4c1e173493efa4",
+    "url": "/PicZippr/index.html"
+  }
+];
